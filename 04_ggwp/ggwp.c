70b512e690235e658912db7104730947dda8da08
Robin Jadoul <robin.jadoul@gmail.com>
2023-02-05 15:50:57 +0100

    04: ggwp

---
+++ 04_ggwp/ggwp.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void setup() {
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stderr, NULL, _IONBF, 0);
+}
+
+void wp(long long secret) {
+    execl(&"asdffdsaadsfadfadfa/bin/sh"[secret], "/bin/sh", NULL);
+}
+
+void gg() {
+    char *questions[] = {"What is your name?", "What is your quest?", "What is the airspeed velocity of an unladen swallow?"};
+    char answer[0x40];
+    for (int i = 0; i < sizeof(questions)/sizeof(questions[0]); i++) {
+        puts(questions[i]);
+        gets(answer);
+        puts("Hmm, I see.");
+    }
+    puts("Aaaaaaaaaaaaa");
+}
+
+int main() {
+    setup();
+    gg();
+}
